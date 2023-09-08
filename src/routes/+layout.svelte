0ce8f319426fe9d0172a5b89b4d774ce27c43cc0
scicluna <88948700+scicluna@users.noreply.github.com>
2023-09-08 16:54:17 -0400

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
   import "../app.css"
   import Drawer from "$lib/components/Drawer.svelte"
   import Foot from "$lib/components/Foot.svelte"
+  import { dev } from "$app/environment"
+  import { inject } from "@vercel/analytics"
+  inject({ mode: dev ? "development" : "production" })
 </script>
 
 <nav class="fixed top-5 left-5 z-50">
